ffbe1f5c0c8d36f9060ca28efa1c0d350f0ad773
unknown <tomerelkm@gmail.com>
2022-05-11 14:59:06 +0300

    adding vue-quill editor

--- frontend/src/components/Feed/NewPost.vue
+++ frontend/src/components/Feed/NewPost.vue
@@ -1,10 +1,27 @@
 <template>
-  <div></div>
+  <div>
+    <quill-editor
+      ref="myQuillEditor"
+      v-model="content"
+      :options="editorOption"
+    />
+  </div>
 </template>
 <script>
+import "quill/dist/quill.core.css";
+import "quill/dist/quill.snow.css";
+import "quill/dist/quill.bubble.css";
+
+import { quillEditor } from "vue-quill-editor";
+
 export default {
   name: "NewPost",
-  data() {},
+  components: { "quill-editor": quillEditor },
+  data() {
+    return {
+      content: "",
+    };
+  },
   methods: {},
   computed: {},
   mounted() {},
