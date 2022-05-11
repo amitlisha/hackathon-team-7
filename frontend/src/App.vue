28c5f69f67e37a240c482526eef7f8d4b6757e75
idoRevah <ido.revah@gmail.com>
2022-05-11 23:27:17 +0300

    router implemented

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,12 @@
 <template>
   <v-app class="app">
-    <app-bar></app-bar>
-    <side-bar/>
-    <v-main>
-      <NewPost />
-    </v-main>
+    <router-view></router-view>
   </v-app>
 </template>
 
 <script>
-import NewPost from "./components/Feed/NewPost.vue";
-import AppBar from "./components/Shell/AppBar.vue"
-import SideBar from './components/Shell/SideBar.vue';
-
 export default {
   name: "App",
-
-  components: {
-    NewPost,
-    AppBar,
-    SideBar
-  },
 
   data: () => ({
     //
