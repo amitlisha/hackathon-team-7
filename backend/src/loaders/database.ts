d0c2ff6a5298c76f7242bc93966cfc4fb7209488
AsafBenHaim <theasf0@gmail.com>
2022-05-12 06:48:19 +0300

    a

--- backend/src/loaders/database.ts
+++ backend/src/loaders/database.ts
@@ -4,7 +4,7 @@ import { Container } from 'typedi';
 
 export default async (): Promise<Connection> => {
   useContainer(Container); 
-  try {
+  try { 
     return await createConnection();
   } catch (err) {
     Logger.error(err);
