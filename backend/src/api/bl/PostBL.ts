2e788d9e40b235862200ee39578619245a3dff29
unknown <tomerelkm@gmail.com>
2022-05-12 10:39:56 +0300

    fixing stuff

--- backend/src/api/bl/PostBL.ts
+++ backend/src/api/bl/PostBL.ts
@@ -5,89 +5,79 @@ import { User } from "../entities/User";
 import pdf from "pdf-parse";
 
 export class PostBL {
-    public static async savePost(
-        title: string,
-        content: string,
-        patientId: number,
-        userId: number,
-        files
-    ) {
-        let textInFiles = "";
-        try {
-            for (let i = 0; i < files.length; i++) {
-                let data = (await pdf(files[i].buffer)).text;
-                data = data.replaceAll(' ', '')
-                textInFiles += data;
-            }
-        } catch (e) { }
+  public static async savePost(
+    title: string,
+    content: string,
+    patientId: number,
+    userId: number,
+    files
+  ) {
+    const postRepository = getRepository(Post);
+    return await postRepository.save({
+      title,
+      content,
+      date: new Date(),
+      patient: { id: patientId },
+      groups: [{ id: userId }],
+    });
+  }
 
-        const postRepository = getRepository(Post);
-        return await postRepository.save({
-            title,
-            content,
-            date: new Date(),
-            patient: { id: patientId },
-            groups: [{ id: userId }],
-            textInFiles
-        });
+  public static async getAll(patientId: number, currentUser: User) {
+    const postRepository = getRepository(Post);
+    const patientRepository = getRepository(Patient);
+    console.log(patientId);
+    const patient = await patientRepository.findOne({
+      where: { id: patientId },
+      relations: ["parent"],
+    });
+    console.log(patient);
+
+    if (patient.parent.id === currentUser.id) {
+      const posts = await postRepository.find({
+        where: { patient: patientId },
+      });
+      return posts;
     }
 
-    public static async getAll(patientId: number, currentUser: User) {
-        const postRepository = getRepository(Post);
-        const patientRepository = getRepository(Patient);
-        console.log(patientId);
-        const patient = await patientRepository.findOne({
-            where: { id: patientId },
-            relations: ["parent"],
-        });
-        console.log(patient);
+    console.log("Hey");
 
-        if (patient.parent.id === currentUser.id) {
-            const posts = await postRepository.find({
-                where: { patient: patientId },
-            });
-            return posts;
-        }
+    const data = await postRepository
+      .createQueryBuilder("post")
+      .select()
+      .leftJoinAndSelect("post.patient", "patient")
+      .leftJoinAndSelect("post.groups", "groups")
+      .leftJoinAndSelect("groups.users", "users")
+      .where("patient.id = :patientId", { patientId })
+      .getMany();
 
-        console.log("Hey");
+    console.log(patientId);
+    console.log(currentUser.id);
+    console.log(data[0]);
+    console.log(data[0].groups[0].users);
 
-        const data = await postRepository
-            .createQueryBuilder("post")
-            .select()
-            .leftJoinAndSelect("post.patient", "patient")
-            .leftJoinAndSelect("post.groups", "groups")
-            .leftJoinAndSelect("groups.users", "users")
-            .where("patient.id = :patientId", { patientId })
-            .getMany();
+    const posts = await postRepository
+      .createQueryBuilder("post")
+      .select()
+      .leftJoin("post.patient", "patient")
+      .leftJoin("post.groups", "groups")
+      .leftJoin("groups.users", "users")
+      .where("users.id = :userId", { userId: currentUser.id })
+      .andWhere("patient.id = :patientId", { patientId })
+      .getMany();
 
-        console.log(patientId);
-        console.log(currentUser.id);
-        console.log(data[0]);
-        console.log(data[0].groups[0].users);
+    console.log(posts);
 
-        const posts = await postRepository
-            .createQueryBuilder("post")
-            .select()
-            .leftJoin("post.patient", "patient")
-            .leftJoin("post.groups", "groups")
-            .leftJoin("groups.users", "users")
-            .where("users.id = :userId", { userId: currentUser.id })
-            .andWhere("patient.id = :patientId", { patientId })
-            .getMany();
+    return posts;
+  }
 
-        console.log(posts);
+  public static async removeGroup(postId: number, groupId: number) {
+    const postRepository = getRepository(Post);
 
-        return posts;
-    }
+    const post = await postRepository.findOne(postId, {
+      relations: ["groups"],
+    });
 
-    public static async removeGroup(postId: number, groupId: number) {
-        const postRepository = getRepository(Post);
-
-        const post = await postRepository.findOne(postId, {
-            relations: ["groups"],
-        });
-
-        post.groups = post.groups.filter((group) => group.id !== groupId);
-        return await postRepository.save(post);
-    }
+    post.groups = post.groups.filter((group) => group.id !== groupId);
+    return await postRepository.save(post);
+  }
 }
--- backend/src/api/entities/Post.ts
+++ backend/src/api/entities/Post.ts
@@ -1,33 +1,39 @@
-import { Column, Entity, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
-import { Document } from './Document';
-import { Group } from './Group';
-import { Patient } from './Patient';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { Document } from "./Document";
+import { Group } from "./Group";
+import { Patient } from "./Patient";
 
-@Entity('post')
+@Entity("post")
 export class Post {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    title: string;
+  @Column()
+  title: string;
 
-    @Column()
-    content: string;
+  @Column()
+  content: string;
 
-    @Column()
-    textInFiles: string;
+  @Column({ type: "timestamp without time zone" })
+  date: Date;
 
-    @Column({ type: 'timestamp without time zone' })
-    date: Date;
+  @OneToMany(() => Document, (document) => document.post)
+  documents: Document[];
 
-    @OneToMany(() => Document, document => document.post)
-    documents: Document[];
+  @ManyToOne((type) => Patient, (patient) => patient.id)
+  @JoinColumn({ name: "patient_id", referencedColumnName: "id" })
+  patient: Patient;
 
-    @ManyToOne((type) => Patient, (patient) => patient.id)
-    @JoinColumn({ name: 'patient_id', referencedColumnName: 'id' })
-    patient: Patient;
-
-    @ManyToMany(() => Group)
-    @JoinTable()
-    groups: Group[]
+  @ManyToMany(() => Group)
+  @JoinTable()
+  groups: Group[];
 }
